9c05ebdec6fb74e30f1298633eb2a7bb2a9f9299
Adrian Kotowski <adekkotowski1@wp.pl>
2025-05-19 16:55:36 +0200

    Add import Routes and Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NotFound from "./components/pages/NotFound/NotFound";
 import { Container } from "react-bootstrap";
 import Home from './components/pages/Home/Home';
 import SingleTable from './components/pages/SingleTable/SingleTable';
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
